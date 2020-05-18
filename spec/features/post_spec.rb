536e5c731c01ed120fd8561b469dc21bc58765fc
Michael Carroll <carrollm2@gmail.com>
2020-05-18 21:00:19 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,7 +6,6 @@ describe 'new post' do
     expect(page.status_code).to eq(200)
   end
 
-
   it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
